9052f3a4f429f9eaec0e22f371c5840768eadbc9
Delex <codedelex@gmail.com>
2025-12-29 19:58:35 +0500

    marts causing error for ci fixed

--- dbt/klineforge_dbt/models/gold/dims/dim_coins.sql
+++ dbt/klineforge_dbt/models/gold/dims/dim_coins.sql
@@ -6,10 +6,7 @@ select distinct
     'USDT'                              as quote_asset, 
     'spot'                              as market_type,
     true                                as is_active,
-<<<<<<< HEAD
     current_timestamp                   as created_at,
     interval
-=======
     current_timestamp                   as created_at
->>>>>>> d2337f45e5b73ec0fc2d9635bd2b953643fc4642
 from {{ ref('fact_1h_klines') }}
